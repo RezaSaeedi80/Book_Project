2072ac9913073448cc84065d19aa99273249bfe1
MrezKord <mohammadrezasaeedi8295@gmail.com>
2022-06-05 11:49:57 +0430

    add fake data

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'price' => strval($this->faker->numberBetween(1000, 20000000)),
+            'isbn' => $this->faker->randomLetter(8)
+        ];
+    }
+}
